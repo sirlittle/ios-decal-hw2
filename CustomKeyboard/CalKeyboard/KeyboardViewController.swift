4bff4ce627b088f2fe435e25381c36917f3061b6
Salil Vanvari <salilvanvari@berkeley.edu>
2015-09-23 23:42:34 -0700

    Finished lol/hi board. upto 10 lols easy :P

--- CustomKeyboard/CalKeyboard/KeyboardViewController.swift
+++ CustomKeyboard/CalKeyboard/KeyboardViewController.swift
@@ -11,15 +11,31 @@ import UIKit
 class KeyboardViewController: UIInputViewController {
 
     @IBOutlet var nextKeyboardButton: UIButton!
+    @IBOutlet var deleteKeyboardButton: UIButton!
+    @IBOutlet var returnKeyboardButton: UIButton!
+    
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var sliderCounter: UILabel!
+    
+    var numRepeat: Int!
     
     var keyboardView: UIView!
 
+    @IBAction func changeLabel(sender: UISlider) {
+        var currentValue = Int(slider.value)
+        sliderCounter.text = "\(currentValue)"
+        numRepeat = currentValue
+    }
+    
     override func updateViewConstraints() {
         super.updateViewConstraints()
     
         // Add custom view sizing constraints here
     }
 
+    @IBAction func printSpace(sender: UIButton) {
+        textDocumentProxy.insertText(" ")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         loadInterface()
@@ -37,7 +53,33 @@ class KeyboardViewController: UIInputViewController {
     override func textDidChange(textInput: UITextInput?) {
         // The app has just changed the document's contents, the document context has been updated.
     }
+    
+    @IBAction func printLol(sender: UIButton) {
+        var proxy = textDocumentProxy
+        for index in 1...numRepeat{
+            proxy.insertText("lol")
+        }
+    }
+    
+    @IBAction func printHi(sender: UIButton) {
+        var proxy = textDocumentProxy
+        for index in 1...numRepeat{
+            proxy.insertText("Hi!")
+        }
+    }
+    
+    @IBAction func del(sender: UIButton) {
+        var proxy = textDocumentProxy
+        proxy.deleteBackward()
+    }
 
+    @IBAction func ret(sender: UIButton) {
+        var proxy = textDocumentProxy
+        proxy.insertText("\n")
+    }
+    
+    
+    
     func loadInterface() {
         let keyboardNib = UINib(nibName: "Keyboard", bundle: nil)
         keyboardView = keyboardNib.instantiateWithOwner(self, options: nil)[0] as! UIView
@@ -46,6 +88,4 @@ class KeyboardViewController: UIInputViewController {
         view.backgroundColor = keyboardView.backgroundColor
         nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside) // advanceToNextInputMode is already defined in template
     }
-
-
 }
